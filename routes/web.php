3d2e73726856d8a0725d3892f1ccb973098b5ed9
Abdo <Qeysabaxa11@gmail.com>
2025-12-25 22:05:55 +0300

    test route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/test', function () {
+    return Inertia::render('test');
+})->name('test');
